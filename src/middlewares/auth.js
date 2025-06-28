9711225ad7a3e0eeb83152005f575e3220944774
ManrajSinghSra <srasinghmanraj@gmail.com>
2025-06-28 14:58:52 +0530

    small Changes+2

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -4,21 +4,23 @@ const User = require("../models/user");
 const Auth=async(req,res,next)=>{
 
     try {
+
+    
         const { token } = req.cookies;  
         if(!token){
             throw new Error("Please login")
         }
-
+      
         const {_id} =  jwt.verify(token, "deujgfgsjw");
         const user = await User.findById(_id);
-c
+
         if (!user) {
           throw new Error("user not exist");
         }
         req.user=user
         next();
     } catch (error) {
-        res.status(400).send(error.message);
+        res.status(400).send(`ERROR : ${error.message}`);
     }
 }
 
--- src/routers/profile.js
+++ src/routers/profile.js
@@ -2,17 +2,18 @@ const express=require("express")
 const Auth=require("../middlewares/auth"); 
 const profileRouter=express.Router()
 const validateEdit=require("../utils/validateEdit")
+const bcrypt=require("bcrypt")
  
 
 profileRouter.get("/profile/view", Auth, (req, res) => {
+
   try {
     const user = req.user;
-    const userCap=user.firstName[0].toUpperCase()+user.firstName.slice(1)
-
-    res.send(`Hello ${userCap} this is`);
+    const userCap=user.displayName()
+    res.send(`Hello ${userCap}`);
   } catch (error) {
-    res.status(400).send(error.message);
-  }
+    res.status(400).send(`ERROR : ${error.message}`);
+  } 
 });
 
 profileRouter.patch("/profile/edit",Auth,async(req,res)=>{
@@ -35,24 +36,34 @@ profileRouter.patch("/profile/edit",Auth,async(req,res)=>{
 
 profileRouter.patch("/profile/password",Auth,async(req,res)=>{
 
-
  try {
   const { previousPass, newpassword } = req.body;
+  if(previousPass==newpassword){
+    res.json({ERROR:"Password cannot be reset"})
+  }
 
   const user = req.user;
-
   const isCorrect = await user.verifyPass(previousPass);
-
   if (!isCorrect) {
     throw new Error("Password do not match");
   }
 
+  if(newpassword.includes(" ")){
+    throw new Error("SPACE IS NOT VALID IN PASSWORD")
+  }
+ 
+
+  const hashPass= await bcrypt.hash(newpassword,10)
+
+  user["password"]=hashPass
+ 
+
+   await user.save();
+
   res.send("password is reseted");
   
  } catch (error) {
-
-  res.send(error.message)
-  
+  res.status(400).send(`ERROR : ${error.message}`);
  }
   
 })
